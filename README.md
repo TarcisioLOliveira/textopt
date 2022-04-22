a241a56d13c41aee129834eb2098ff7792944a3f
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-04-21 23:05:22 -0300

    Initial commit
    Add basic CMake structure.
    
    Add SFML as dependency.
    
    Add basic documentation files and license.

---
+++ README.md
@@ -0,0 +1,6 @@
+# textopt - Texture optimization
+
+Optimizes machining parameters to maximize surface area while minimizing surface roughness.
+
+Dependency:
+- SFML
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of textopt.
+ *
+ *   Foobar is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <SFML/Window.hpp>
+
+int main(int argc, char* argv[]){
+
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+
+    return 0;
+}
