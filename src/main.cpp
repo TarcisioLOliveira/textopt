ce774c992ed74dfd6a54e20e1360fc1cfae34c22
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-04-24 21:55:30 -0300

    Add basic rendering
    Make SFML render grayscale image which changes at every iteration.
    
    Make image not stretch or shrink with window resize.

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,59 @@
  *
  */
 
+#include <SFML/Config.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <random>
+
+size_t tex_width = 300;
+size_t tex_height = 300;
 
 int main(int argc, char* argv[]){
 
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    size_t window_width = 800;
+    size_t window_height = 600;
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "textopt");
+    sf::Texture img;
+    img.create(tex_width, tex_height);
 
+    std::default_random_engine generator;
+    std::uniform_int_distribution<sf::Uint8> rand(0);
+    sf::Uint8* px = new sf::Uint8[tex_width*tex_height*4];
+    sf::Sprite sprite(img);
+    sprite.setPosition(sf::Vector2f(window_width/2-tex_width/2, window_height/2-tex_height/2));
+
+    while(window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+            if(event.type == sf::Event::Resized){
+                sf::FloatRect view(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(view));
+            }
+        }
+        window.clear(sf::Color::Black);
+        for(size_t i = 0; i < 300*300; ++i){
+            sf::Uint8 p = rand(generator);
+            px[4*i+0] = p;
+            px[4*i+1] = p;
+            px[4*i+2] = p;
+            px[4*i+3] = 255;
+        }
+        img.update(px);
+        auto wsize = window.getSize();
+        window_width = wsize.x;
+        window_height = wsize.y;
+        sprite.setPosition(sf::Vector2f(window_width/2-tex_width/2, window_height/2-tex_height/2));
+
+        window.draw(sprite);
+        window.display();
+    }
 
     return 0;
 }
