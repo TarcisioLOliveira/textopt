1d1587667ac7edc50a735ab9438bb1b05252a960
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-05-03 21:07:36 -0300

    Add triangle_area()
    Calculates the area of a triangle from 3 points using a vectorial
    method.

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,19 @@ struct Vector{
     double x, y, z;
 };
 
+double triangle_area(std::array<Point, 3> p){
+    Vector v1{p[1].x - p[0].x, p[1].y - p[0].y, p[1].z - p[0].z};
+    Vector v2{p[2].x - p[0].x, p[2].y - p[0].y, p[2].z - p[0].z};
+
+    double A = 0.5*std::sqrt(
+        std::pow(v1.y*v2.z - v2.y*v1.z, 2) +
+        std::pow(v1.x*v2.z - v2.x*v1.z, 2) +
+        std::pow(v1.y*v2.x - v2.y*v1.x, 2)
+    );
+
+    return A;
+}
+
 double smooth_min(std::initializer_list<double> x){
     double mult = -8;
     double frac_top = 0;
