a9ce4825a35290a14a24e4260c727e23d32ec861
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-05-03 22:58:17 -0300

    Add derivative functions for feed rate (untested)

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ double alpha = 60*M_PI/180;
 double r = 2; // um
 double max_z = 0; // Used to calculate Sa, so uses smooth_min
 double min_z = 0; // Used only for texture display, so uses std::min()
+double dmax_zdf = 0; 
                   
 struct Point{
     double x, y, z;
@@ -63,6 +64,25 @@ double triangle_area(std::array<Point, 3> p){
     return A;
 }
 
+
+double triangle_area_deriv(std::array<Point, 3> p, std::array<double, 3> dz){
+    Vector v1{p[1].x - p[0].x, p[1].y - p[0].y, p[1].z - p[0].z};
+    Vector v2{p[2].x - p[0].x, p[2].y - p[0].y, p[2].z - p[0].z};
+    double dv1 = dz[1] - dz[0];
+    double dv2 = dz[2] - dz[0];
+
+    double A = (0.5/std::sqrt(
+        std::pow(v1.y*v2.z - v2.y*v1.z, 2) +
+        std::pow(v1.x*v2.z - v2.x*v1.z, 2) +
+        std::pow(v1.y*v2.x - v2.y*v1.x, 2)
+    ))*(2*(v1.y*v2.z - v2.y*v1.z)*(v1.y*dv2 - v2.y*dv1) +
+        2*(v1.x*v2.z - v2.x*v1.z)*(v1.x*dv2 - v2.x*dv1) +
+        0
+    );
+
+    return A;
+}
+
 double smooth_min(std::initializer_list<double> x){
     double mult = -8;
     double frac_top = 0;
@@ -74,6 +94,21 @@ double smooth_min(std::initializer_list<double> x){
     }
 
     return frac_top/frac_bot;
+};
+
+double smooth_min_deriv(std::initializer_list<double> x, double xi){
+    double mult = -8;
+    double frac_top = 0;
+    double frac_bot = 0;
+    for(double xi : x){
+        double exp = std::exp(mult*xi);
+        frac_top += xi*exp;
+        frac_bot += exp;
+    }
+
+    double sm = frac_top/frac_bot;
+
+    return (xi/frac_bot)*(1+mult*(xi-sm));
 };
 
 void texture_map(double*& map_z, double f, double ap, double vc){
@@ -104,6 +139,38 @@ void texture_map(double*& map_z, double f, double ap, double vc){
     }
 }
 
+
+void dzdf(double*& map_z, double f, double ap, double vc, double*& dzdf){
+    double line_root = ap - std::tan(alpha)*f/2;
+    double dlrdf = -std::tan(alpha)/2;
+    max_z = smooth_min({0, -line_root});
+    dmax_zdf = smooth_min_deriv({0, -line_root}, -line_root)*dlrdf;
+    min_z = 0;
+    for(size_t x = 0; x < tex_width; ++x){
+        size_t mult = 1;
+        for(size_t y = 0; y < tex_height; ++y){
+            double& z = map_z[tex_width*y + x];
+            double& dz = dzdf[tex_width*y + x];
+            if(y < mult*f){
+                if(y <= (mult-1)*f + f/2){
+                    // z = smooth_min({z, -std::tan(alpha)*(y - (mult-1)*f) - line_root});
+                    double znew = -std::tan(alpha)*(y - (mult-1)*f) - line_root;
+                    double dznewdf = std::tan(alpha)*(mult-1) - dlrdf;
+                    dz = smooth_min_deriv({z, znew}, znew)*dznewdf;
+                } else {
+                    // z = smooth_min({z, std::tan(alpha)*(y - ((mult-1)*f + f)) - line_root});
+                    double znew = -std::tan(alpha)*(y - (mult-1)*f + f) - line_root;
+                    double dznewdf = std::tan(alpha)*mult - dlrdf;
+                    dz = smooth_min_deriv({z, znew}, znew)*dznewdf;
+                }
+            } else {
+                ++mult;
+                --y;
+            }
+        }
+    }
+}
+
 void draw_texture(sf::Uint8*& img, double* map_z, double ap, size_t w, size_t h){
     for(size_t x = 0; x < tex_width; ++x){
         for(size_t y = 0; y < tex_height; ++y){
@@ -122,6 +189,14 @@ double Sa(double*& map_z){
     size_t area = tex_width*tex_height;
     double sum = std::abs(std::accumulate(map_z, map_z+area, 0.0, std::plus<double>()));
     sum += area*max_z; // Depth correction
+
+    return sum/area;
+}
+
+double dSadf(double*& map_z, double* dzdf){
+    size_t area = tex_width*tex_height;
+    double sum = std::abs(std::accumulate(dzdf, dzdf+area, 0.0, std::plus<double>()));
+    sum += area*dmax_zdf; // Depth correction
 
     return sum/area;
 }
@@ -168,6 +243,50 @@ double surface_area(double*& map_z){
     return A;
 }
 
+double surface_area_df(double*& map_z, double* dzdf){
+    double A = 0;
+    for(size_t x = 0; x < tex_width; x+=2){
+        for(size_t y = 0; y < tex_height; y+=2){
+            Point p[9];
+            double dz[9];
+            size_t blockw = std::min(3ul, tex_width-x);
+            size_t blockh = std::min(3ul, tex_height-y);
+            if(blockw <= 1 || blockh <= 1){
+                continue;
+            }
+            for(size_t i = 0; i < blockw; ++i){
+                for(size_t j = 0; j < blockh; ++j){
+                    p[i*3+j] = Point{
+                        dimx*(x+i),
+                        dimy*(y+j),
+                        dimz*map_z[tex_width*(y+j) + (x+i)]
+                    };
+                    dz[i*3+j] = dzdf[tex_width*(y+j) + (x+i)];
+                }
+            }
+
+            A += triangle_area_deriv({p[0], p[1], p[4]}, {dz[0], dz[1], dz[4]});
+            A += triangle_area_deriv({p[0], p[3], p[4]}, {dz[0], dz[3], dz[4]});
+
+            if(blockw == 3){
+                A += triangle_area_deriv({p[1], p[2], p[4]}, {dz[1], dz[2], dz[4]});
+                A += triangle_area_deriv({p[2], p[4], p[5]}, {dz[2], dz[4], dz[5]});
+            }
+            if(blockh == 3){
+                A += triangle_area_deriv({p[3], p[4], p[6]}, {dz[3], dz[4], dz[6]});
+                A += triangle_area_deriv({p[4], p[6], p[7]}, {dz[4], dz[6], dz[7]});
+            }
+            if(blockw == 3 && blockh == 3){
+                A += triangle_area_deriv({p[4], p[5], p[8]}, {dz[4], dz[5], dz[8]});
+                A += triangle_area_deriv({p[4], p[7], p[8]}, {dz[4], dz[7], dz[8]});
+            }
+        }
+    }
+
+    return A;
+
+}
+
 int main(int argc, char* argv[]){
 
     size_t window_width = 800;
@@ -182,6 +301,7 @@ int main(int argc, char* argv[]){
     // std::uniform_int_distribution<sf::Uint8> rand(0);
     sf::Uint8* px = new sf::Uint8[tex_width*tex_height*4]();
     double* map_z = new double[tex_width*tex_height]();
+    double* dzdf = new double[tex_width*tex_height]();
 
     double f = 30;
     double ap = 40;
