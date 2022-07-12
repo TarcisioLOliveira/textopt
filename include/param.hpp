ee88e132f7c82de2a44032aa135fe5c858e6e3e8
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-07-12 16:23:22 -0300

    Fix copyright notice

--- include/param.hpp
+++ include/param.hpp
@@ -14,7 +14,7 @@
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with textopt.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
