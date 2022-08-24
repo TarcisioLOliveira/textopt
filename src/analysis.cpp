ca1e611bb86cd7d2e682dea12f9e18e8cb2ec7d6
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-08-23 22:55:41 -0300

    Make plot_* functions cache to vectors first
    It was an attempt to make it faster, but doesn't work out too well.
    Still, it's better than resizing a stringstream repeatedly.

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -30,20 +30,36 @@ namespace analysis{
 void plot_fxap(const std::tuple<double, double>& f, const std::tuple<double, double>& ap, const double vc, const double step, std::vector<double>& map_z, const std::vector<double>& orig_z){
     // Cache resulting string into memory, as it's faster than continually
     // writing to disk
-    std::stringstream result;
 
-    const double diff = std::get<1>(f) - std::get<0>(f);
+    const double diff_f = std::get<1>(f) - std::get<0>(f);
+    const double diff_ap = std::get<1>(ap) - std::get<0>(ap);
+    const size_t size = (diff_f/step + 1)*(diff_ap/step + 1);
+    std::vector<double> results(size*2);
+    auto r = results.begin();
+
     std::cout << "Calculating plot points..." << std::endl;
     for(double fi = std::get<0>(f); fi <= std::get<1>(f)+1e-7; fi += step){
-        std::cout << "\r" << (fi - std::get<0>(f))*100/diff << "%      " << std::flush;
+        std::cout << "\r" << (fi - std::get<0>(f))*100/diff_f << "%      " << std::flush;
         for(double api = std::get<0>(ap); api <= std::get<1>(ap)+1e-7; api += step){
             texture::map(map_z, orig_z, fi, api, vc);
             const double surarea = opt::surface_area(map_z);
             const double roughness = opt::Sa(map_z);
-            result << surarea << " " << roughness << std::endl;
+            *r = surarea;
+            ++r;
+            *r = roughness;
+            ++r;
         }
     }
     std::cout << "\r100%      " << std::endl;
+    std::string resstr;
+    resstr.reserve(results.size()*25);
+    std::stringstream result(resstr);
+    r = results.begin();
+    while(r < results.end()){
+        result << *r << " " << *(r+1) << std::endl;
+        r += 2;
+    }
+
     std::ofstream file;
     file.open("plot_fxap.txt");
     // Write ranges
@@ -58,17 +74,31 @@ void plot_fxap(const std::tuple<double, double>& f, const std::tuple<double, dou
 void plot_vc(const double f, const double ap, const std::tuple<double, double>& vc, const double step, std::vector<double>& map_z, const std::vector<double>& orig_z){
     // Cache resulting string into memory, as it's faster than continually
     // writing to disk
-    std::stringstream result;
-
     const double diff = std::get<1>(vc) - std::get<0>(vc);
+    const size_t size = (diff/step + 1);
+    std::vector<double> results(size*2);
+    auto r = results.begin();
+
     std::cout << "Calculating plot points..." << std::endl;
     for(double vci = std::get<0>(vc); vci <= std::get<1>(vc)+1e-7; vci += step){
         std::cout << "\r" << (vci - std::get<0>(vc))*100/diff << "%      " << std::flush;
         texture::map(map_z, orig_z, f, ap, vci);
         const double surarea = opt::surface_area(map_z);
         const double roughness = opt::Sa(map_z);
-        result << surarea << " " << roughness << std::endl;
+        *r = surarea;
+        ++r;
+        *r = roughness;
+        ++r;
     }
+    std::string resstr;
+    resstr.reserve(results.size()*25);
+    std::stringstream result(resstr);
+    r = results.begin();
+    while(r < results.end()){
+        result << *r << " " << *(r+1) << std::endl;
+        r += 2;
+    }
+
     std::cout << "\r100%      " << std::endl;
     std::ofstream file;
     file.open("plot_vc.txt");
