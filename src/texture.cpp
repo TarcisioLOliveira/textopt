b821433d7fd5704a2d64fcf9a88ef4d01c801e05
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2022-12-09 16:07:42 -0300

    texture: update dzdap

--- src/texture.cpp
+++ src/texture.cpp
@@ -548,113 +548,195 @@ void dzdap(const std::vector<double>& orig_z, double f, double ap, double vc, st
 
     vc *= 1e6/60.0;
 
-    double yc = (tan2*f+b2off-b1off)/(tan1+tan2);
+    // Calculate maximum width
+    const double ap1 = -std::sqrt(r*r - param::y1*param::y1) + r;
+    const double ap2 = -std::sqrt(r*r - param::y2*param::y2) + r;
+    const double ap12min = std::min(ap1, ap2);
+    const double ap12max = std::max(ap1, ap2);
 
-    double s1 = -yc;
-    double s2 = f-yc;
-    if(alpha1 < alpha2 && s2 < y2){
-        const double a = tan1*tan1 + 1;
-        const double b = -2*(tan1*(r-b1off)+f);
-        const double c = b1off*(b1off-2*r)+f*f;
-        yc = (-b - std::sqrt(b*b - 4*a*c))/(2*a);
-    } else if(alpha2 < alpha1 && s1 > y1){
-        const double a = tan2*tan2 + 1;
-        const double b = -(2*tan2*(b2off-r)+2*tan2*tan2*f);
-        const double c = tan2*tan2*f*f + 2*tan2*f*(b2off-r) + b2off*(b2off-2*r);
-        yc = (-b + std::sqrt(b*b - 4*a*c))/(2*a);
-    }
-
-    double h = 0;
-    s1 = -yc;
-    s2 = f-yc;
-    if(s1 > y1 && s2 < y2){
-        yc = f/2;
-        h = -std::sqrt(r*r - yc*yc) + r;
-    } else if(s1 > y1){
-        h = tan2*(f-yc) + b2off;
-    } else if(s2 < y2){
-        h = tan1*yc + b1off;
+    double dw_max;
+    if(ap < ap12min){
+        // Depth fits entirely within tool radius
+        w_max = 2*std::sqrt(r*r - (ap - r)*(ap - r));
+        dw_max = 2*(-(ap - r))/std::sqrt(r*r - (ap - r)*(ap - r));
+    } else if(ap < ap12max){
+        // Depth fits partially within tool radius
+        // Can only happen for alpha1 != alpha2
+        w_max = std::sqrt(r*r - (ap - r)*(ap - r));
+        dw_max = 2*(-(ap - r))/std::sqrt(r*r - (ap - r)*(ap - r));
+        if(alpha1 <= alpha2){
+            w_max += (ap - b1off)/tan1;
+            dw_max += 1.0/tan1;
+        } else {
+            w_max += (ap - b2off)/tan2;
+            dw_max += 1.0/tan2;
+        }
     } else {
-        h = tan1*yc + b1off;
+        // Depth encompasses tool's straight edges
+        w_max = (ap - b1off)/tan1 + (ap - b2off)/tan2;
+        dw_max = 1.0/tan1 + 1.0/tan2;
     }
+
+    // This will not be differentiated, so it can be use std::floor()
+    const size_t overlap = std::floor(w_max / f);
 
     const double delta_uet = vc/f_uet;
 
-    const double over_z = Az + Az_uet*(1.0 - std::sqrt(1.0 - (delta_uet*delta_uet)/(4*Ax_uet*Ax_uet)));
+    min_z = -ap;
 
-    // min_z = -(ap + Az);
-    dmax_zdap = smooth::min_deriv({0, h+min_z+over_z+Az}, 1)*(-1);
+    const double line_root1_const = -std::sqrt(r*r - y1*y1) + r - ap + tan1*y1;
+    const double line_root2_const = -std::sqrt(r*r - y2*y2) + r - ap - tan2*y2;
+    const double dline_root1_const = -1;
+    const double dline_root2_const = -1;
 
-    const double line_root1 = -std::sqrt(r*r - y1*y1) + r - ap + tan1*(yc+y1);
-    const double line_root2 = -std::sqrt(r*r - y2*y2) + r - ap - tan2*(yc+y2);
+    const double perimeter = 2*M_PI*cylinder_radius;
 
-    const double dlrdap1 = -1;
-    const double dlrdap2 = -1;
+    std::vector<double> z_cand(overlap + 2, 0);
+    std::vector<double> dz_cand(overlap + 2, 0);
 
-    #pragma omp parallel
-    {
-        std::vector<double> newz(tex_width);
-        std::vector<double> dznewdap(tex_width);
-        #pragma omp for
-        for(size_t Y = 0; Y < tex_height; ++Y){
-            const double y = static_cast<double>(Y);
+    #pragma omp parallel for firstprivate(z_cand,dz_cand)
+    for(size_t Y = 0; Y < tex_height; ++Y){
+        const double y = static_cast<double>(Y);
 
-            const double mult = smooth::abs(smooth::floor( (y + YOFF) / f));
+        // Calculate current row
+        const double _mult = smooth::floor((y - 0.5*w_max)  / f + 1);
+        const double _dmult = smooth::floor_deriv((y - 0.5*w_max)  / f + 1)*(-0.5*dw_max);
 
-            const double perimeter = 2*M_PI*(cylinder_radius - max_z);
-            const double dperimeter = -2*M_PI*dmax_zdap;
+        for(size_t X = 0; X < tex_width; ++X){
+            const double x = static_cast<double>(X);
 
-            const double xoffset_uet = mult*perimeter;
-            const double dxoff_uet = mult*dperimeter;
+            z_cand[0] = orig_z[X + Y*tex_width];
 
-            for(size_t X = 0; X < tex_width; ++X){
-                const double x = static_cast<double>(X);
+            for(size_t m = 0; m < overlap + 1; ++m){
+                // Overlap
+                const double mult = _mult + m;
+                const double dmult = _dmult;
 
+                // Distance travelled by tool
+                const double xoffset_uet = mult*perimeter;
+                const double dxoffset_uet = dmult*perimeter;
+
+                // Consider distance travelled by tool
                 const double xcirc = x + xoffset_uet;
-                const double dxcirc = dxoff_uet;
+                const double dxcirc = dxoffset_uet;
 
-                const double oscillation = Az*std::sin(2*M_PI*fz*xcirc*dimx/vc + phiz);
-                const double doscilldap = Az*std::cos(2*M_PI*fz*xcirc*dimx/vc + phiz)*2*M_PI*fz*dxcirc*dimx/vc;
-
-                const double mult_uet = smooth::abs(smooth::floor(xcirc / delta_uet));
-                const double dmult_uet = smooth::abs_deriv(smooth::floor(xcirc / delta_uet))*smooth::floor_deriv(xcirc / delta_uet)*dxcirc/delta_uet;
+                // Get distance relative to period
+                const double mult_uet = smooth::floor(xcirc / delta_uet);
+                const double dmult_uet = smooth::floor_deriv(xcirc / delta_uet)*dxcirc/delta_uet;
 
                 const double x_uet = xcirc - (mult_uet + 0.5)*delta_uet;
                 const double dx_uet = dxcirc - dmult_uet*delta_uet;
 
-                const double uet_effect = Az_uet*(1.0 - std::sqrt(1.0 - (x_uet*x_uet)/(Ax_uet*Ax_uet)));
-                const double duet = -0.5*(Az_uet/std::sqrt(1.0 - (x_uet*x_uet)/(Ax_uet*Ax_uet)))*(-2)*(x_uet/Ax_uet)*(dx_uet/Ax_uet);
+                // Tool path
+                double z_uet = 0;
+                double dz_uet = 0;
+                if(vc <= v_crit) {
+                    // If vc <= v_crit, model as ellipses in series
+                    const double H = Az_uet*std::sin(M_PI*vc/(2*v_crit));
+                    const double h1 =  H + Az_uet;
+                    const double h2 = -H + Az_uet;
+                    const double K = delta_uet*(h1+h2)/(2*(h1-h2));
+                    const double delta_1 =  K + delta_uet/2;
 
-                newz[X] = oscillation + uet_effect;
-                dznewdap[X] = doscilldap + duet;
+                    z_uet = h1*(1.0 - std::sqrt(1.0 - (4*x_uet*x_uet)/(delta_1*delta_1)));
+                    const double ell = std::sqrt(1.0 - (4*x_uet*x_uet)/(delta_1*delta_1));
+                    dz_uet = h1*((4*x_uet*dx_uet)/(delta_1*delta_1))/ell;
+                } else {
+                    // If otherwise, model as alternating ellipses with
+                    // different dimensions.
+                    //
+                    // Also interpolate the ellipses with a senoidal model in
+                    // order to achieve better representation of the tool
+                    // path, especially as Ax_uet tends to 0
+                    const double H = Az_uet*std::sin(M_PI*v_crit/(2*vc));
+                    const double h1 =  H + Az_uet;
+                    const double h2 = -H + Az_uet;
+                    const double K = delta_uet*(h1-h2)/(2*(h1+h2));
+                    const double delta_1 =  K + delta_uet/2;
+                    const double delta_2 = -K + delta_uet/2;
+
+                    double z_uet1 = 0;
+                    double z_uet2 = 0;
+                    double dz_uet1 = 0;
+                    double dz_uet2 = 0;
+                    if(x_uet < -delta_1/2){
+                        const double x_uet2 = x_uet + delta_uet/2;
+                        const double dx_uet2 = dx_uet;
+
+                        z_uet1 = h1 + h2*std::cos(M_PI*x_uet2/delta_2);
+                        dz_uet1 = -h2*std::sin(M_PI*x_uet2/delta_2)*M_PI*dx_uet2/delta_2;
+
+                        z_uet2 = h1 + h2*std::sqrt(1.0 - (4*x_uet2*x_uet2)/(delta_2*delta_2));
+                        const double ell = std::sqrt(1.0 - (4*x_uet2*x_uet2)/(delta_2*delta_2));
+                        dz_uet = h2*((4*x_uet2*dx_uet2)/(delta_2*delta_2))/ell;
+                    } else if(x_uet < delta_1/2){
+                        const double x_uet2 = x_uet;
+                        const double dx_uet2 = dx_uet;
+
+                        z_uet1 = h1*(1.0 - std::cos(M_PI*x_uet2/delta_1));
+                        dz_uet1 = h1*std::sin(M_PI*x_uet2/delta_1)*M_PI*dx_uet2/delta_1;
+
+                        z_uet2 = h1*(1.0 - std::sqrt(1.0 - (4*x_uet2*x_uet2)/(delta_1*delta_1)));
+                        const double ell = std::sqrt(1.0 - (4*x_uet2*x_uet2)/(delta_1*delta_1));
+                        dz_uet = -h1*((4*x_uet2*dx_uet2)/(delta_1*delta_1))/ell;
+                    } else {
+                        const double x_uet2 = x_uet - delta_uet/2;
+                        const double dx_uet2 = dx_uet;
+
+                        z_uet1 = h1 + h2*std::cos(M_PI*x_uet2/delta_2);
+                        dz_uet1 = -h2*std::sin(M_PI*x_uet2/delta_2)*M_PI*dx_uet2/delta_2;
+
+                        z_uet2 = h1 + h2*std::sqrt(1.0 - (4*x_uet2*x_uet2)/(delta_2*delta_2));
+                        const double ell = std::sqrt(1.0 - (4*x_uet2*x_uet2)/(delta_2*delta_2));
+                        dz_uet = h2*((4*x_uet2*dx_uet2)/(delta_2*delta_2))/ell;
+                    }
+    
+                    const double v_ratio = v_crit/vc;
+                    const double dv_ratio = -v_crit/(vc*vc);
+
+                    const double rat = std::sqrt(1 - v_ratio*v_ratio);
+                    z_uet = z_uet1*std::sqrt(1 - v_ratio*v_ratio) + z_uet2*(1 - std::sqrt(1 - v_ratio*v_ratio));
+                    dz_uet = dz_uet1*rat + z_uet1*(-v_ratio*dv_ratio)/rat + dz_uet2*(1 - rat) - z_uet2*(v_ratio*dv_ratio)/rat;
+                }
+
+                // Clearance angle
+                const double x_uet_c = xcirc + 0.5*delta_uet - smooth::floor(xcirc / delta_uet + 0.5)*delta_uet;
+                const double dx_uet_c = dxcirc  - smooth::floor_deriv(xcirc / delta_uet + 0.5)*dxcirc;
+
+                const double z_clear = tanc*(delta_uet - x_uet_c);
+                const double dz_clear = tanc*(delta_uet - dx_uet_c);
+
+                // Final UET height
+                const double z_uet_min = smooth::min({z_uet, z_clear});
+                const double dz_uet_min = smooth::min_deriv({z_uet, z_clear},{dz_uet, dz_clear});
+
+                const double line_root1 = line_root1_const + z_uet_min;
+                const double line_root2 = line_root2_const + z_uet_min;
+                const double dline_root1 = dline_root1_const + dz_uet_min;
+                const double dline_root2 = dline_root2_const + dz_uet_min;
+
+                // Tool shape
+                double shape_z;
+                double dshape_z;
+                if(y <= y1 + mult*f){
+                    shape_z = -tan1*(y - mult*f) + line_root1;
+                    dshape_z = dline_root1;
+                } else if(y <= y2 + mult*f){
+                    const double yy = y - mult*f;
+                    shape_z = -std::sqrt(r*r - yy*yy) + r - ap + z_uet_min;
+                    dshape_z = -1.0 + dz_uet_min;
+                } else {
+                    shape_z = tan2*(y - mult*f) + line_root2;
+                    dshape_z = dline_root2;
+                }
+
+                z_cand[m+1] = shape_z;
+                dz_cand[m+1] = dshape_z;
             }
-
-            double shape_z;
-            double dshape_z;
-            if(y <= y1 + mult*f + yc){
-                shape_z = -tan1*(y - mult*f) + line_root1;
-                dshape_z = dlrdap1;
-            } else if(y <= y2 + mult*f + yc){
-                const double yy = y - mult*f - yc;
-
-                const double circ = std::sqrt(r*r - yy*yy);
-
-                shape_z = -circ + r - ap;
-                dshape_z = -1;
-            } else {
-                shape_z = tan2*(y - mult*f) + line_root2;
-                dshape_z = dlrdap2;
-            }
-
-            for(size_t X = 0; X < tex_width; ++X){
-                const double oz = orig_z[X + Y*tex_width];
-                double& dz = dzdap[X + Y*tex_width];
-
-                const double end_z = newz[X] + shape_z;
-                const double dend_z = dznewdap[X] + dshape_z;
-
-                dz = smooth::min_deriv({oz, end_z}, 1)*dend_z;
-            }
+            // Get pixels
+            double& dz = dzdap[X + Y*tex_width];
+            // Write results
+            dz = smooth::min_deriv(z_cand,dz_cand);
         }
     }
 }
