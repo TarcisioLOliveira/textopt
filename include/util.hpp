939ad84748a197e0aee7af4c56a701a291168295
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2023-10-29 15:08:35 -0300

    util: fix cross product calculation

--- include/util.hpp
+++ include/util.hpp
@@ -57,9 +57,9 @@ inline double triangle_area(const std::array<Point, 3>& p){
     const Vector v1{p[1].x - p[0].x, p[1].y - p[0].y, p[1].z - p[0].z};
     const Vector v2{p[2].x - p[0].x, p[2].y - p[0].y, p[2].z - p[0].z};
 
-    const double expr1 = v1.y*v2.z - v2.y*v1.z;
-    const double expr2 = v1.x*v2.z - v2.x*v1.z;
-    const double expr3 = v1.y*v2.x - v2.y*v1.x;
+    const double expr1 =  v1.y*v2.z - v1.z*v2.y;
+    const double expr2 = -v1.x*v2.z + v1.z*v2.x;
+    const double expr3 =  v1.x*v2.y - v1.y*v2.x;
 
     const double A = 0.5*std::sqrt(
         expr1*expr1 +
@@ -86,16 +86,16 @@ inline double triangle_area_deriv(const std::array<Point, 3>& p, const std::arra
     const double dv1 = dz[1] - dz[0];
     const double dv2 = dz[2] - dz[0];
 
-    const double expr1 = v1.y*v2.z - v2.y*v1.z;
-    const double expr2 = v1.x*v2.z - v2.x*v1.z;
-    const double expr3 = v1.y*v2.x - v2.y*v1.x;
+    const double expr1 =  v1.y*v2.z - v1.z*v2.y;
+    const double expr2 = -v1.x*v2.z + v1.z*v2.x;
+    const double expr3 =  v1.x*v2.y - v1.y*v2.x;
 
     const double A = (0.25/std::sqrt(
         expr1*expr1 +
         expr2*expr2 +
         expr3*expr3
-    ))*(2*(v1.y*v2.z - v2.y*v1.z)*(v1.y*dv2 - v2.y*dv1) +
-        2*(v1.x*v2.z - v2.x*v1.z)*(v1.x*dv2 - v2.x*dv1) +
+    ))*(2*(v1.y*v2.z - v2.y*v1.z)*( v1.y*dv2 - dv1*v2.y) +
+        2*(v1.x*v2.z - v2.x*v1.z)*(-v1.x*dv2 + dv1*v2.x) +
         0
     );
 
